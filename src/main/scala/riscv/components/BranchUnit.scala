8fcaa8df6ee854bbbf69f28187f76cabaafedaa0
freddyTheAlch <s220768@dtu.dk>
2026-07-18 20:03:09 +0200

    BranchUnit

--- src/main/scala/riscv/components/BranchUnit.scala
+++ src/main/scala/riscv/components/BranchUnit.scala
@@ -2,16 +2,26 @@ package riscv.components
 
 import chisel3._
 import chisel3.util._
-
-
-
-import riscv.isa.
+import riscv.isa.Opcodes
 
 class BranchUnit extends Module {
   val io = IO(new Bundle() {
     val a = Input(UInt(32.W))
     val b = Input(UInt(32.W))
-    val branch = Input(Bool)
+    val branch = Input(Bool())
+    val funct3 = Input(UInt(3.W))
 
+    val takeBranch = Output(Bool())
   })
+
+  io.branch := false.B
+
+  switch(io.funct3){
+    is(Opcodes.BEQ) {io.takeBranch := io.a === io.b}
+    is(Opcodes.BNE) {io.takeBranch := io.a =/= io.b}
+    is(Opcodes.BLT) {io.takeBranch := io.a.asSInt < io.b.asSInt}
+    is(Opcodes.BGE) {io.takeBranch := io.a.asSInt >= io.b.asSInt}
+    is(Opcodes.BLTU) {io.takeBranch := (io.a < io.b)}
+    is(Opcodes.BGEU) {io.takeBranch := (io.a >= io.b).asUInt}
+  }
 }
--- src/main/scala/riscv/isa/Opcodes.scala
+++ src/main/scala/riscv/isa/Opcodes.scala
@@ -1,14 +1,12 @@
-package riscv.components
+package riscv.isa
 
 import chisel3._
 
-object OPS {
-  val BEQ = 0.U(3.W)
-  val BNE = 1.U(3.W)
-  val BLT = 2.U(3.W)
-  val BGE = 3.U(3.W)
-  val BLTU = 4.U(3.W)
-  val BGEU = 5.U(3.W)
-  val JAL = 6.U(3.W)
-  val JALR = 7.U(3.W)
+object Opcodes {
+  val BEQ = 0.U(3.W) // Branch Equal
+  val BNE = 1.U(3.W) // Branch Not Equal
+  val BLT = 2.U(3.W) // Branch Less Than
+  val BGE = 3.U(3.W) // Branch Greater Than Or Equal
+  val BLTU = 4.U(3.W) // Branch Less Than Unsigned
+  val BGEU = 5.U(3.W) // Branch Greater Than Or Equal Unsigned
 }
